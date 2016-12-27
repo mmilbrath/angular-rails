ca61ac9eeb5fbb74c9f5df7719b26938ed9d2c30
Michael Milbrath <m.milbrath@gmail.com>
2016-12-27 13:22:03 -0500

    udemy rails/angular course

---
+++ freelance_camp_proposal/app/mailers/proposal_mailer.rb
@@ -0,0 +1,8 @@
+class ProposalMailer < ApplicationMailer
+
+  def email(proposal)
+    @proposal = proposal
+    mail(to: @proposal.client_email, subject: "You've received a new proposal")
+  end
+
+end
